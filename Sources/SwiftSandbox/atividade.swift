7fe4a8ecec2c5bc8d6027c784f9d7c5a6229a509
JoaoArtur151 <arturfreitaspessoa@icloud.com>
2026-08-21 15:01:04 -0300

    consertar os prints

--- Sources/SwiftSandbox/atividade.swift
+++ Sources/SwiftSandbox/atividade.swift
@@ -1,5 +1,3 @@
-let cards = [1, 2, 3]
-
 /// Obtém a carta no índice especificado da pilha.
 ///
 /// - Parameters:
@@ -8,7 +6,11 @@ let cards = [1, 2, 3]
 /// - Returns: A carta localizada no índice especificado.
 func getCard(at index: Int, from stack: [Int]) -> Int {
   // Implemente a função getCard(at:from:)
-    return stack[index]
+    if index < stack.count{
+        return stack[index]
+    } else {
+        return -1
+    }
 }
 
 /// Substitui a carta existente no índice especificado por uma nova carta.
@@ -19,8 +21,14 @@ func getCard(at index: Int, from stack: [Int]) -> Int {
 ///   - newCard: A nova carta que substituirá a carta existente.
 /// - Returns: Uma nova pilha com a carta no índice especificado substituída por `newCard`.
 func setCard(at index: Int, in stack: [Int], to newCard: Int) -> [Int] {
-  // Implemente a função setCard(at:from:to:)
-    
+    if index < stack.count {
+        var novaPilha = stack
+        novaPilha[index] = newCard
+        return novaPilha
+    } else {
+        print("Operação falhou, indice", index, "invalido")
+        return stack
+    }
 }
 
 /// Insere uma nova carta no topo da pilha.
@@ -31,6 +39,9 @@ func setCard(at index: Int, in stack: [Int], to newCard: Int) -> [Int] {
 /// - Returns: Uma nova pilha com a carta inserida no topo.
 func insert(_ newCard: Int, atTopOf stack: [Int]) -> [Int] {
   // Implemente a função insert(_:atTopOf:)
+    var novaPilha = stack
+    novaPilha.insert(newCard, at: 0)
+    return novaPilha
 }
 
 /// Remove a carta no índice especificado da pilha.
@@ -41,6 +52,9 @@ func insert(_ newCard: Int, atTopOf stack: [Int]) -> [Int] {
 /// - Returns: Uma nova pilha sem a carta removida.
 func removeCard(at index: Int, from stack: [Int]) -> [Int] {
   // Implemente a função removeCard(at:from:)
+    var novaPilha = stack
+    novaPilha.remove(at: index)
+    return novaPilha
 }
 
 /// Insere uma nova carta no índice especificado da pilha.
@@ -51,7 +65,10 @@ func removeCard(at index: Int, from stack: [Int]) -> [Int] {
 ///   - stack: A pilha que contém as cartas.
 /// - Returns: Uma nova pilha com a carta inserida no índice especificado.
 func insert(_ newCard: Int, at index: Int, from stack: [Int]) -> [Int] {
-  // Implemente a função insert(_:at:from:)
+    // Implemente a função insert(_:at:from:)
+    var novaPilha = stack
+    novaPilha.insert(newCard, at: index)
+    return novaPilha
 }
 
 /// Verifica se a pilha possui a quantidade especificada de cartas.
@@ -62,4 +79,13 @@ func insert(_ newCard: Int, at index: Int, from stack: [Int]) -> [Int] {
 /// - Returns: `true` se a pilha tiver a quantidade especificada de cartas; caso contrário, `false`.
 func checkSizeOfStack(_ stack: [Int], _ size: Int) -> Bool {
   // Implemente a função checkSizeOfStack(_:_)
+    return stack.count == size
 }
+
+let cards = [1, 2, 3]
+
+let card = getCard(at: 1, from: cards)
+print(cards)
+
+let card2 = setCard(at: 3, in: cards, to: 12)
+print(card2)
